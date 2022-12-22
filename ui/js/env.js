16fa78072acdfd1d32fea6ffc20e83ff526e40f6
Matias Naakka <matiasnaakka.matias5230@gmail.com>
2022-12-22 12:48:22 +0200

    update

--- ui/js/env.js
+++ ui/js/env.js
@@ -1,5 +1,5 @@
 const env = {
   // TODO: change url when uploading to server "https://users.metropolia.fi/~veetiso/Heat-or-not"
-  baseUrl: 'http://:3000',
+  baseUrl: 'http://10.114.34.63/app',
 
 }
